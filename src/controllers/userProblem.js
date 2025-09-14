1879f8e6caff9e009da44d1f5d33e1723198cad1
Gauhar Alam <gauhar54995@gmail.com>
2025-09-14 15:41:43 +0530

    Refactor createProblem logic and update problem routes for better clarity and organization

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -26,7 +26,6 @@ const createProblem = async (req, res) => {
 
       const languageId = getLanguageById(language);
       // I am creating Batch submission
-            // I am creating Batch submission
       const submissions = visibleTestCases.map((testcase) => ({
         source_code: completeCode, 
         language_id: languageId,
@@ -38,18 +37,7 @@ const createProblem = async (req, res) => {
       // console.log(submitResult);
 
       const resultToken = submitResult.map((value) => value.token);
-
-      // ["db54881d-bcf5-4c7b-a2e3-d33fe7e25de7","ecc52a9b-ea80-4a00-ad50-4ab6cc3bb2a1","1b35ec3b-5776-48ef-b646-d5522bdeb2cc"]
-
       const testResult = await submitToken(resultToken);
-
-      //  console.log(testResult);
-
-      console.log(testResult)
-
-      // console.log(submissions,"sumission");
-
-
       if (!submitResult) {
         return res.status(500).send("No response from Judge0");
       }
@@ -59,15 +47,16 @@ const createProblem = async (req, res) => {
       if (!Array.isArray(submissionsArray)) {
         return res.status(500).send("Invalid response format from Judge0");
       }
-
-
-      // ["db54881d-bcf5-4c7b-a2e3-d33fe7e25de7","ecc52a9b-ea80-4a00-ad50-4ab6cc3bb2a1","1b35ec3b-5776-48ef-b646-d5522bdeb2cc"]
-      testResult.forEach((result)=>{
-        console.log("id->>>>>>>>>>>>>>>>>>>",result.status_id); 
-        if(result.status.id!=3 && result.status.description==='Accepted'){
-          return res.status(400).json({message:result.status.description, language:result.language.name, status_id: result.status_id});
+      
+      for (const result of testResult) {
+        if (result.status.id !== 3) {
+          return res.status(400).json({
+            message: `Test case failed: ${result.status.description}`,
+            language: result.language.name,
+            status_id: result.status.id,
+          });
         }
-      })
+      }
 
         try {
           const userProblem = await Problem.create({
@@ -133,7 +122,7 @@ const updateProblem = async (req, res) => {
 
       //  console.log(testResult);
 
-      console.log(testResult)
+      // console.log(testResult)
       for (const test of testResult) {
         if (test.status_id != 3) {
           return res.status(400).send("Error Occured");
@@ -212,70 +201,70 @@ const updateProblem = async (req, res) => {
 //   }
 // };
 
-const deleteProblem = async(req,res)=>{
+// const deleteProblem = async(req,res)=>{
 
-  const {id} = req.params;
+//   const {id} = req.params;
 
-  try{
-    if(!id){
-      return res.status(400).send("ID is Missing");
-    }
+//   try{
+//     if(!id){
+//       return res.status(400).send("ID is Missing");
+//     }
 
-    const deletedProblem = await Problem.findByIdAndDelete(id);
+//     const deletedProblem = await Problem.findByIdAndDelete(id);
 
-    if(!deleteProblem){
-      res.status(404).send("Problem is Missing");
-    }
+//     if(!deleteProblem){
+//       res.status(404).send("Problem is Missing");
+//     }
 
-    res.status(200).send("Deleted Succesfully");
-  }
-  catch(err){
-    res.status(500).send("Error"+err);
-  }
-};
+//     res.status(200).send("Deleted Succesfully");
+//   }
+//   catch(err){
+//     res.status(500).send("Error"+err);
+//   }
+// };
 
-const getProblemById = async(req,res)=>{
+// const getProblemById = async(req,res)=>{
 
-  const {id} = req.params;
+//   const {id} = req.params;
 
-  try{
-    if(!id){
-      res.status(400).send("ID is Missing");
-    }
+//   try{
+//     if(!id){
+//       res.status(400).send("ID is Missing");
+//     }
 
-    const getProblem = await Problem.findById(id).select('_id title description difficulty tags visibleTestCases startCode referenceSolution');
+//     const getProblem = await Problem.findById(id).select('_id title description difficulty tags visibleTestCases startCode referenceSolution');
 
-    if(!getProblem){
-      res.status(404).send("Problem is Missing");
-    }
+//     if(!getProblem){
+//       res.status(404).send("Problem is Missing");
+//     }
 
-    res.status(200).send(getProblem);
-  }
-  catch(err){
-    res.status(500).send("Error"+err);
-  }
+//     res.status(200).send(getProblem);
+//   }
+//   catch(err){
+//     res.status(500).send("Error"+err);
+//   }
 
-}
+// }
 
-const getAllProblem = async(req,res)=>{
+// const getAllProblem = async(req,res)=>{
 
-  try{
-  const allProbem = await Problem.find({}).select('_id title difficulty tags');
+//   try{
+//   const allProbem = await Problem.find({}).select('_id title difficulty tags');
 
-  if(allProbem.length===0){
-    res.status(404).send("Problems is Missing");
-  }
+//   if(allProbem.length===0){
+//     res.status(404).send("Problems is Missing");
+//   }
 
-  res.status(200).send(allProbem);
-  }
-  catch(err){
-    res.status(500).send("Error"+err);
-  }
+//   res.status(200).send(allProbem);
+//   }
+//   catch(err){
+//     res.status(500).send("Error"+err);
+//   }
 
-};
+// };
 
-const solvedAllProblemByUser  = async(req,res)=>{
+// const solvedAllProblemByUser  = async(req,res)=>{
 
-}
+// }
 
-module.exports = { createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem };
+module.exports = { createProblem,updateProblem};
