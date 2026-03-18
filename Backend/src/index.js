78123667bde238104844fd8a0b8f38574a60eed8
Gauhar Alam <gauharalam@Gauhars-MacBook-Air.local>
2026-03-19 03:19:39 +0530

    Add Netlify redirects and configure deployment variables

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,7 @@ const submitRouter = require("./routes/submit");
 const cors = require("cors");
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:5001'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:5001', process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
